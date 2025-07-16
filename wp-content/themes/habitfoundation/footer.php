56df9e21b07707d2472a8e20048f199d416b9278
shivtejimg <shivanibatham12.img@gmail.com>
2025-07-16 19:03:59 +0530

    update home page

---
+++ wp-content/themes/habitfoundation/footer.php
@@ -0,0 +1,5 @@
+
+<?php echo do_shortcode('[habit_footer]'); ?>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/main.js"></script>
+</body>
+</html>
---
+++ wp-content/themes/habitfoundation/index.php
@@ -0,0 +1,6 @@
+<?php get_header(); ?>
+<main class="py-8 px-4">
+  <h2>Default Template</h2>
+  <p>This is the fallback index.php template.</p>
+</main>
+<?php get_footer(); ?>
