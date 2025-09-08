3e545d98482ae9f8ef91f5bf240342993f34e6d2
shivtejimg <shivanibatham12.img@gmail.com>
2025-09-08 19:02:08 +0530

    update nurturing-innovation page

--- wp-content/themes/habitfoundation/connect-us.php
+++ wp-content/themes/habitfoundation/connect-us.php
@@ -4,7 +4,7 @@
 <section class="w-full bg-[#f6f5f2] relative lg:py-16 md:py-8 py-6">
     <div class="max-w-7xl mx-auto px-4 px-4 lg:px-6">
         <div class="w-full flex flex-col gap-y-6">
-            
+
             <div class="bg-[#f6f5f2] flex flex-col items-start justify-between rounded-xl mb-4 lg:py-10 lg:px-7 py-5 px-4 gap-8">
                 <iframe 
                     src="https://docs.google.com/forms/d/e/1FAIpQLSelCAGP3Na186LblXgiHAPiwVhQdno0CeFr7JjwGThjfnRsqw/viewform?usp=sharing&ouid=114688243916935856866" 
