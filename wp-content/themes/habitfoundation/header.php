3e9c5e05a23ee8c26dd07cb9f648512de1e48715
shivtejimg <shivanibatham12.img@gmail.com>
2025-09-29 11:58:43 +0530

    Update header.php

--- wp-content/themes/habitfoundation/header.php
+++ wp-content/themes/habitfoundation/header.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5, shrink-to-fit=no"/>
     
-    <link rel="pingback" href="https://habitfoundation.in/xmlrpc.php" />
-	<meta name='robots' content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
+    <meta name='robots' content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
     <!-- Fonts & Scripts -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/custom.css"/>
 	<link href="https://fonts.googleapis.com/css2?family=Heebo:wght@100..900&family=Raleway:ital,wght@0,100..900;1,100..900&family=Quicksand:wght@300..700&family=Lora:ital,wght@0,400..700;1,400..700&family=Playfair:ital,opsz,wght@0,5..1200,300..900;1,5..1200,300..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
